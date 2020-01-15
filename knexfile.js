4aa6e25dd63e6e462bee61e84192d3f28c15e246
Michael Williams <manicdesigner@gmail.com>
2020-01-15 10:51:35 -0500

    add db3 to knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3',
+      filename: './data/login.db3',
     },
   },
   pool: {
