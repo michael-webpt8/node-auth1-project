6b10392d6d585a0b82b5f60d6ea87fc08db38b61
Michael Williams <manicdesigner@gmail.com>
2020-01-15 09:29:52 -0500

    add index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res, next) => {
+  res.json({
+    message: 'Welcome to our API',
+  });
+});
+
+server.use((err, req, res, next) => {
+  console.log('Error:', err);
+
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
+
+server.listen(port, () => {
+  console.log(`\n** Running on http://localhost:${port} **\n`);
+});
